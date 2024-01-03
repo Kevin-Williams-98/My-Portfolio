b27be4c1b18f9f0e1587aa3d90487f94cc0d8d2e
Kevin Williams <k.king.will.k@gmail.com>
2024-01-02 23:56:05 -0500

    Health check serverless function

--- api/server.js
+++ api/server.js
@@ -49,3 +49,7 @@ export default function handler(request, response) {
     });
   });
 }
+
+export function healthCheck(request, response) {
+  return response.json({ code: 200, status: 'Health is good' });
+}
